7c51ec385b20f2e131bfa63dbf735b8af50e6934
wcrespo <wellingtoncrespo2001@gmail.com>
2023-11-26 17:30:44 -0500

    creando rutas de products

--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
 const express = require('express')
+const product = require('./json/products.json')
 
 const app = express();
 
 const PORT = process.env.PORT ?? 5000
 
-app.get('/', (req, res)=>{
-    res.send('hola')
+app.get('/products', (req, res)=>{
+    res.json(product)
 });
+
+app.post('/newproducts', (req, res)=>{
+    let body = '';
+
+    req.on('data', chunk =>{
+        body += chunk.toString() 
+    })
+
+    req.on('end', ()=>{
+        const data = JSON.parse(body)
+
+        data.timestamp = Date.now()
+        res.status(201).json(data)
+    })
+})
+
 
 
 
