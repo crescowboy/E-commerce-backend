2f2fab5c4fb44cf0001751a0830ed809bca77bfe
wcrespo <wellingtoncrespo2001@gmail.com>
2023-11-26 23:02:32 -0500

    agg al endpoit filtrar por tipo

--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
 const express = require('express')
-const product = require('./json/products.json')
+const products = require('./json/products.json')
 
 const app = express();
-
+app.disable('x-powered-by')
 const PORT = process.env.PORT ?? 5000
 
+
 app.get('/products', (req, res)=>{
-    res.json(product)
+    const { type } = req.query
+    if(type){
+        const filteredType = products.filter(
+            product => product.type.some(g => g.toLowerCase() === type.toLowerCase())
+        )
+        return res.json(filteredType)
+    }
+    res.json(products)
 });
+
+app.get('/products/:id', (req, res)=>{
+    const {id} = req.params
+    const product = products.find(product => product.id = id) 
+    if(product) return res.json(product)
+
+    res.status(404).json({message: "No se encontro el producto"})
+})
 
 app.post('/newproducts', (req, res)=>{
     let body = '';
