4ad4f4d4e855ef1b0ff9268de13d92dc4c1e855c
wcrespo <wellingtoncrespo2001@gmail.com>
2023-11-30 12:52:10 -0500

    Agg acepted origins

--- server/index.js
+++ server/index.js
@@ -11,7 +11,16 @@ const PORT = process.env.PORT ?? 5000
 // Middleware para analizar el cuerpo de la solicitud JSON
 app.use(express.json());
 
+const ACCEPTED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:5000'
+]
+
 app.get('/products', (req, res)=>{
+    const origin = req.header('origin')
+    if(ACCEPTED_ORIGINS.includes(origin) || !origin){
+        res.header('Access-Control-Allow-Origin', origin)
+    }
     const { type } = req.query
     if(type){
         const filteredType = products.filter(
